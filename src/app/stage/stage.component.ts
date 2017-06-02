035ac5f99b27b31876cfad08d35eddcf26bbc335
Radek Sohlich <sohlich@gmail.com>
2017-06-02 21:58:05 +0200

    migration to bootstrap

--- src/app/stage/stage.component.ts
+++ src/app/stage/stage.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Task } from './../model/task'
 import { RepositoryService } from './../service/repository.service'
+import { TaskFormComponent } from "app/task-form/task-form.component";
+import { EditAction } from "app/model/edit-action.enum";
+import { TaskEdit } from "app/model/taskedit";
+import { MdDialog } from "@angular/material";
 
 @Component({
   selector: 'app-stage',
@@ -12,11 +16,11 @@ export class StageComponent implements OnInit {
   @Input() title: string;
   @Input() columnWidth: number;
 
-  constructor(private _storage: RepositoryService) {
+  constructor(private _storage: RepositoryService,public dialog: MdDialog) {
   }
 
   ngOnInit() {
-    
+
   }
 
   public acceptDrop(event) {
@@ -25,7 +29,25 @@ export class StageComponent implements OnInit {
     this._storage.addTask(data);
   }
 
-  
+
+  public onAddTask(event: any) {
+    console.log("Adding task.");
+    
+    let dialogRef = this.dialog.open(TaskFormComponent, {
+      width: '50vw',
+      height: '25vh',
+      data: new TaskEdit(EditAction.NEW, null)
+    });
+
+    let obs = dialogRef.afterClosed().subscribe(result => {
+      if (result !== "OK") { return; }
+      let task = dialogRef._containerInstance.dialogConfig.data.data;
+      task.created = new Date().getTime();
+      task.stage = this.title;
+      this._storage.addTask(task);
+      obs.unsubscribe();
+    });
+  }
 
 
 }
